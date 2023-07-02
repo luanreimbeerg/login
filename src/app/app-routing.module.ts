b381fe5fca4e5e7ab575802ef98bcf75e6c5c651
Luan Trajano <luan.trajano@biz.com.br>
2023-07-02 19:46:52 -0300

    feat(app-component): add path signin

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'signin',
+    loadChildren: () =>
+      import('./pages/login/login.module').then((m) => m.LoginModule),
+  },
+  { path: '', redirectTo: 'signin', pathMatch: 'full' },
+  { path: '**', redirectTo: 'signin', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
