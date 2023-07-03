95883c3ff837f3338098f071e3826e49db494e38
Luan Trajano <luan.trajano@biz.com.br>
2023-07-02 22:11:57 -0300

    feat(login): add html login

--- src/app/pages/login/login.module.ts
+++ src/app/pages/login/login.module.ts
@@ -1,17 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 
 import { LoginRoutingModule } from './login-routing.module';
 import { LoginComponent } from './login.component';
 
-
 @NgModule({
-  declarations: [
-    LoginComponent
-  ],
+  declarations: [LoginComponent],
   imports: [
     CommonModule,
-    LoginRoutingModule
-  ]
+    LoginRoutingModule,
+    MatInputModule,
+    MatIconModule,
+    MatButtonModule,
+  ],
 })
-export class LoginModule { }
+export class LoginModule {}
